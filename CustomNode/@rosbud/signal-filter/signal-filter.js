3eee1566b7cd51bd24fe1c589bbe5cbeca78d80d
WonKyu Jeong <wonkyu.rosbud.jung@gmail.com>
2022-03-06 22:00:27 +0900

    .
    .

--- CustomNode/@rosbud/signal-filter/signal-filter.js
+++ CustomNode/@rosbud/signal-filter/signal-filter.js
@@ -1,7 +1,7 @@
 module.exports = function (RED) {
   function getField(node, kind, value) {
     switch (kind) {
-      case 'flow':	// Legacy
+      case 'flow': // Legacy
         return node.context().flow.get(value);
       case 'global':
         return node.context().global.get(value);
@@ -20,10 +20,11 @@ module.exports = function (RED) {
   function signalFilter(config) {
     var node = this;
     RED.nodes.createNode(node, config);
-    node.topic = config.topic;
+    node.filter = config.filter;
+    console.log(node.filter)
 
     node.on('input', function (msg) {
-      msg.payload = msg.payload.toLowerCase();
+      // msg.payload = msg.payload.toLowerCase();
       node.send(msg);
     });
   }
